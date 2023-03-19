79b63f45671db5947a23031322a15f2e5ac891e0
Kalyan Ch <32354220+kalyancheerla@users.noreply.github.com>
2023-03-18 23:44:37 -0500

    updated to use JSON data instead

--- Samples/DigraphConverter/Program.cs
+++ Samples/DigraphConverter/Program.cs
@@ -28,48 +28,9 @@ namespace DigraphConverter
                 string input_file = args[0];
                 string output_file = args[1];
 
-                // create a dict to use for data flow
-                Dictionary<string, Guid> processDict = new Dictionary<string, Guid>();
-
-                int index = 0;
-                foreach (string line in File.ReadLines(input_file))
-                {
-                    string pattern1 = @"^(\d+)\s*\[\s*label\s*=\s*""([^""]*)""\s*\]$";
-                    string pattern2 = @"^(\d+)\s*->\s*(\d+)\s*\[constraint=(\w+)\]$";
-
-                    Match match = Regex.Match(line, pattern1);
-                    if (match.Success)
-                    {
-                        var id = match.Groups[1].Value;
-                        var label = match.Groups[2].Value;
-
-                        // add process entities and then in diagram
-                        var process = model.AddEntity<IProcess>(id);
-                        diagram.AddShape(process, new PointF(10*id.Length, 100*index));
-
-                        // add guids in dict
-                        processDict.Add(id, process.Id);
-                    }
-
-                    match = Regex.Match(line, pattern2);
-                    if (match.Success)
-                    {
-                        var sourceNode = match.Groups[1].Value;
-                        var destNode = match.Groups[2].Value;
-
-                        // add dataflow in model and then in diagram
-                        IDataFlow dataFlow = model.AddDataFlow("Flow", processDict[sourceNode], processDict[destNode]);
-                        diagram.AddLink(dataFlow);
-                    }
-                    index++;
-                }
-
-                // Save the model to a file in JSON format.
-                var fileName = "sample_threat_model.tm";
-                var json = ThreatModelManager.Serialize(model);
-                var package = Package.Create(fileName);
-                package.Add("threatmodel.json", json);
-                package.Save();
+                // deserialize JSON directly from a file
+                var microsvcs = new MicroSvcsLoader().LoadJSONFile(input_file);
+                new MicroSvcsLoader().PrettyPrint(microsvcs);
             }
         }
     }
